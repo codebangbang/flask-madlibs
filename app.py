b5ba45e8f256523606bc37059f5250e4e3a7a8a5
Ryan <ryfair@gmail.com>
2023-12-21 12:35:02 -0500

    1 extra space when returning my story choice is screwing the whole thing up...

--- app.py
+++ app.py
@@ -1,66 +1,30 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-# from stories import story
+from stories import stories
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 debug = DebugToolbarExtension(app)
 
 @app.route("/")
-def questions(): 
+def select_story():
+    return render_template("select-story.html", stories = stories.values())
+
+
+@app.route("/questions")
+def ask_questions(): 
     """Request words from user"""
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
     prompts = story.prompts
-    return render_template("questions.html", prompts=prompts)
+
+    return render_template("questions.html", story_id=story_id, title=story.title,prompts=prompts)
 
 @app.route("/story")
 def show_story():
     """return completed story"""  
+    story_id=request.args["story_id"]
+    story = stories[story_id]
     text = story.generate(request.args)
-    return render_template("story.html", text=text)
-
-"""Madlibs Stories."""
-
-
-class Story:
-    """Madlibs story.
-
-    To  make a story, pass a list of prompts, and the text
-    of the template.
-
-        >>> s = Story(["noun", "verb"],
-        ...     "I love to {verb} a good {noun}.")
-
-    To generate text from a story, pass in a dictionary-like thing
-    of {prompt: answer, promp:answer):
-
-        >>> ans = {"verb": "eat", "noun": "mango"}
-        >>> s.generate(ans)
-        'I love to eat a good mango.'
-    """
-
-    def __init__(self, words, text):
-        """Create story with words and template text."""
-
-        self.prompts = words
-        self.template = text
-
-    def generate(self, answers):
-        """Substitute answers into text."""
-
-        text = self.template
-
-        for (key, val) in answers.items():
-            text = text.replace("{" + key + "}", val)
-
-        return text
-
-
-# Here's a story to get you started
-
-
-story = Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-)
-
+    return render_template("story.html", title=story.title, text=text)
